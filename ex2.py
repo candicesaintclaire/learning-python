55d9743b5846d08f3c873921a789a87125829c6d
Candice Saint Claire <cstcl@pm.me>
2026-02-09 00:54:25 -0800

    Remove root-level exercise files (now tracked under chapters folders)

--- ex2.py
+++
@@ -1,9 +0,0 @@
-# A comment, this is so you can read your program later.
-# Anything after the pound sign (#) is ignored by the python interpreter.
-
-print("I could have code like this.") # and the comment after is ignored
-
-# You can also use a comment to disable or comment out code:
-# print("This won't run.")
-
-print("But this will.")
